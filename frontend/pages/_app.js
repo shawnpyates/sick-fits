67136e41e843001014be27e7f452dc5918cfba83
Shawn Yates <yates.shawnp@gmail.com>
2018-11-04 18:26:51 -0800

    enables item queries and mutations on react side

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,31 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // exposes query to user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
