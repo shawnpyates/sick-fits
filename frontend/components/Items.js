20c774085587d959397cd180f7fdf2d4f2839465
Shawn Yates <yates.shawnp@gmail.com>
2018-11-12 17:25:27 -0800

    pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+
 import Item from './Item';
+import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -32,11 +35,15 @@ const ItemsList = styled.div`
 
 class Items extends Component {
   render() {
+    const { page } = this.props;
     return (
       <Center>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{ skip: (page * perPage) - perPage }}
+        >
           {({ data, error, loading }) => {
-            console.log('data: ', data);
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
             return (
@@ -46,6 +53,7 @@ class Items extends Component {
             );
           }}
         </Query>
+        <Pagination page={page} />
       </Center>
     );
   }
