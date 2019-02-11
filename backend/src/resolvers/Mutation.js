8c76a51a89a7a0b8e97c96aa79b391b9946534fb
Shawn Yates <yates.shawnp@gmail.com>
2019-02-10 16:43:10 -0800

    addItems mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -174,7 +174,35 @@ const Mutations = {
         id: args.userId,
       }
     }, info);
-  }
+  },
+  async addToCart(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be signed in.');
+    }
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      },
+    });
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 },
+      }, info);
+    }
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: {
+          connect: { id: args.id },
+        }
+      },
+    }, info)
+  },
 };
 
 module.exports = Mutations;
